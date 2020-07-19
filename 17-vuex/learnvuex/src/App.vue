52e22669940c174cb710b6ef9507fdfac5394c0d
WH <419264755@qq.com>
2020-07-19 19:54:16 +0800

    vuex

---
+++ 17-vuex/learnvuex/src/App.vue
@@ -0,0 +1,47 @@
+<template>
+  <div id="app">
+
+    <h2>--------app内容---------</h2>
+    <h2>{{message}}</h2>
+    <h2>{{$store.state.counter}}</h2>
+    <button @click="addition">+</button>
+    <button @click="subtraction">-</button>
+
+    <h2>--------app内容：getters相关内容---------</h2>
+    <h2>{{$store.getters.powerCounter}}</h2>
+
+    <h2>--------hello vuex内容---------</h2>
+    <hello-vuex />
+  </div>
+</template>
+
+<script>
+
+import HelloVuex from './components/HelloVuex'
+
+export default {
+  name: 'App',
+  components: {
+    HelloVuex
+  },
+  data(){
+    return{ 
+      message:'我是app组件'
+    }
+  },
+  methods: {
+    addition() {
+      console.log('addition');
+      this.$store.commit('increment')
+    },
+    subtraction() {
+      console.log('subtraction');
+      this.$store.commit('decrement')
+    }
+  }
+  
+}
+</script>
+
+<style>
+</style>
---
+++ 17-vuex/learnvuex/src/store/index.js
@@ -0,0 +1,36 @@
+import Vue from  'vue'
+import Vuex from 'vuex'
+
+
+// 1.安装插件
+Vue.use(Vuex)
+
+// 2.创建对象
+const store = new Vuex.Store({
+    state: {
+        counter: 1000
+    },
+    mutations: {
+        //方法
+        increment(state){
+            state.counter++
+        },
+        decrement(state){
+            state.counter--
+        }
+    },
+    actions: {
+
+    },
+    getters: {
+        powerCounter(state){
+           return state.counter * state.counter
+        }
+    },
+    modules: {
+
+    }
+})
+
+// 3.导出store独享
+export default store
