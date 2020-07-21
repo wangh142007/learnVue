c7c336666bd4e9a1be9236893a048515f1093f51
wxl <422421011@qq.com>
2020-07-21 16:55:41 +0800

    mall

--- 19-project/mall/src/router/index.js
+++ 19-project/mall/src/router/index.js
@@ -1,6 +1,11 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 
+const originalReplace = Router.prototype.replace;
+Router.prototype.replace = function replace(location) {
+  return originalReplace.call(this, location).catch(err => err);
+};
+
 const Home = () =>import('@/views/home/Home') 
 const Category = () =>import('@/views/category/Category') 
 const Cart = () =>import('@/views/cart/Cart') 
